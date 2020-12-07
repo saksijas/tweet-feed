b83a9cae65a38cffbbab2e1573428389f65464af
Mustafa Mahmutovic <mustafa.mahmutovic93@gmail.com>
2020-12-07 14:07:08 +0100

    Fixed issue with mapping tweet with username

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,15 +6,16 @@ async function getUser(username) {
     if (user) {
         const friends = await User.find({ username: user.following }, { username: 1, tweets: 1 })
         let tweets = []
-        const tweetsMapper = (tweet) => {
-            tweets.push({ user: user.username, content: tweet })
+        const tweetsMapper = (tweet, username) => {
+            tweets.push({ user: username, content: tweet })
         }
-        user.tweets.map(tweetsMapper)
+        user.tweets.map(tweet => tweetsMapper(tweet, user.username))
 
         // Going through list of friends mapping each tweet with username
-        friends.forEach((friend) => friend.tweets.map(tweetsMapper))
+        friends.forEach((friend) => friend.tweets.map(tweet => tweetsMapper(tweet, friend.username)))
         return { user, tweets }
     }
+    // Will return null and error will be thrown
     return user
 }
 async function getUsers() {
